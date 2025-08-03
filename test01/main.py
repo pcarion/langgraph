597cdcff08ae22db5a25ca9d17374d939d99723b
Pierre Carion <pcarion@gmail.com>
2025-08-03 16:16:30 -0700

    first chatbot

--- test01/main.py
+++ test01/main.py
@@ -7,6 +7,8 @@ from langgraph.graph.message import add_messages
 import os
 from langchain.chat_models import init_chat_model
 
+llm = init_chat_model("anthropic:claude-3-5-sonnet-latest")
+
 
 class State(TypedDict):
     # Messages have the type "list". The `add_messages` function
@@ -16,3 +18,38 @@ class State(TypedDict):
 
 
 graph_builder = StateGraph(State)
+
+def chatbot(state: State):
+    return {"messages": [llm.invoke(state["messages"])]}
+
+# The first argument is the unique node name
+# The second argument is the function or object that will be called whenever
+# the node is used.
+graph_builder.add_node("chatbot", chatbot)
+
+graph_builder.add_edge(START, "chatbot")
+
+
+graph_builder.add_edge("chatbot", END)
+
+graph = graph_builder.compile()
+
+def stream_graph_updates(user_input: str):
+    for event in graph.stream({"messages": [{"role": "user", "content": user_input}]}):
+        for value in event.values():
+            print("Assistant:", value["messages"][-1].content)
+
+
+while True:
+    try:
+        user_input = input("User: ")
+        if user_input.lower() in ["quit", "exit", "q"]:
+            print("Goodbye!")
+            break
+        stream_graph_updates(user_input)
+    except:
+        # fallback if input() is not available
+        user_input = "What do you know about LangGraph?"
+        print("User: " + user_input)
+        stream_graph_updates(user_input)
+        break
